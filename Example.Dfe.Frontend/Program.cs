3dde938554b197062e2f96b921ae7c7557ec67d1
Zach Hodges <zach.hodges@hippodigital.co.uk>
2026-02-06 10:32:49 +0000

    first

---
+++ Example.Dfe.Frontend/Program.cs
@@ -0,0 +1,34 @@
+using GovUk.Frontend.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddGovUkFrontend();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseGovUkFrontend();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+    .WithStaticAssets();
+
+app.Run();
